dc12f245448cf3760027d710b1c3afb5ad653c62
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-29 21:45:33 +0530

    News

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ var input = document.querySelector(".search");
 var btn1 = document.querySelector(".submitButton");
 let container = document.querySelector(".container");
 btn1.addEventListener("click", function (event) {
-  fetch(`https://newsapi.org/v2/everything?q=${input.value}&from=2022-07-27&sortBy=publishedAt&apiKey=846fd227de084ee2ae29d9474c3e1a43`) //fetching api data
-    .then((response) => response.json()) //text --> json
+  fetch(`https://newsapi.org/v2/everything?q=${input.value}&from=2022-07-25&sortBy=publishedAt&apiKey=7f0bf7b97fdf4346b992decbdaa0c15b`)
+ //fetching api data
+    .then((response) => response.json()) //text --> json    
     .then((data) => {
-      let res = data["articles"];
-      //console.log(res)
-      for (let i = 0; i < 10; i++) { 
-        let article = res[i];
-        // console.log(article)
-        let html = `<div>
-                    <img src=${article["urlToImage"]}>  
-                    <h1>${article["title"]} </h1>
-                    <p>${article["author"]}</p>
-                    <p>${article["description"]} <a href=${article["url"]}>Read more</a> </p>
-                  </div>`;
+      let res = data["articles"]; 
+      console.log(data)
+      // for (let i = 0; i < 10; i++) {
+      //   let article = res[i];
+      //   //  console.log(article)
+        // let html = `<div>
+        //             <img src=${article["urlToImage"]}>  
+        //             <h1>${article["title"]} </h1>
+        //             <p>${article["author"]}</p>
+        //             <p>${article["description"]} <a href=${article["url"]}>Read more</a> </p>
+        //           </div>`;
 
-        container.innerHTML += html;
-      }
+        //container.innerHTML += html;
+  //}
     })
     .catch((err) => alert("Nothing found"));
 });
